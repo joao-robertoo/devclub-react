9fbd82356170ba312d188ac1ccdc1ce679602c72
J <joao.adsbrasil@gmail.com>
2026-01-08 03:27:37 -0300

    Iniciando a criação da tela de cadastro do usuário

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,47 @@
-import { Title, Container } from './styles'
+import { Title, Container, TopBackground, Form, ContainerInputs, Button, InputLabel } from './styles'
 
-export function App() {
+function Home() {
 
   return (
-      <Container>
-        <Title>Olá React</Title>
-        
-      </Container>
+    <Container>
+      <TopBackground >
+        <img src="" alt="" />
+      </TopBackground>
+
+      <Form>
+        <Title>Cadastrar Usuário</Title>
+        <ContainerInputs>
+          <div>
+
+            <div>
+              <InputLabel>
+                Nome<span>*</span>
+              </InputLabel>
+              <Input type="text" placeholder="Nome do Usuário" />
+            </div>
+
+            <div>
+              <InputLabel>
+                Idade<span>*</span>
+              </InputLabel>
+              <Input Type="number" placeholder="Idade do Usuário" />
+            </div>
+            
+          </div>
+
+            <div>
+              <InputLabel>
+                Email<span>*</span>
+              </InputLabel>
+              <Input type="email" placeholder="E-mail do Usuário" />
+            </div>
+
+        </ContainerInputs>
+        <Button>Cadastrar Usuário</Button>
+      </Form>
+
+    </Container>
   )
 }
+
+export default Home
--- src/styles.js
+++ src/styles.js
@@ -1,6 +1,10 @@
 import styled from 'styled-components'
 
-export const Container = styled.div`
-    
-
-`
+export const Container = styled.div``
+export const Title = styled.h2``
+export const TopBackground = styled.div``
+export const Form = styled.form``
+export const ContainerInputs = styled.div``
+export const Input = styled.input``
+export const InputLabel = styled.label``
+export const Button = styled.button``
