85bc2e148495528845b95b061dbff54dfd0b6927
J <joao.adsbrasil@gmail.com>
2026-01-08 00:33:11 -0300

    JSX e a estrutura de um componente REACT

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+// import App from './App.jsx' // default
+import { App, App2 } from './App.jsx' // export se, default
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  <React.StrictMode>
     <App />
-  </StrictMode>,
+  </React.StrictMode>,
 )
