72766e2deb9ad529f3723e90752d47ae59282ac8
J <joao.adsbrasil@gmail.com>
2026-01-10 00:59:18 -0300

    Entendendo o conteito de rotas no react

---
+++ src/routes.jsx
@@ -0,0 +1,17 @@
+import { createBrowserRouter } from 'react-router-dom'
+
+import Home from './pages/Home'
+import ListUsers from './pages/ListUsers'
+
+const router = createBrowserRouter ([
+    {
+        path: '/',
+        element: <Home />
+    },
+    {
+        path: '/lista-de-usuarios',
+        elente: <ListUsers />
+    }
+])
+
+export default Router
